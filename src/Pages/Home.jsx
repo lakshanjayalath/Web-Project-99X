631a7faad37ab66f4af98dafd94d3325eaa80ce9
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-05-31 10:17:16 +0530

    add homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import { Box, Typography, Button, Container } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import HomeMenu from '../components/HomeMenu';
+import '../css/HomePage.css'
 
 const Home = () => {
   return (
-    <div>
-        <Header />
-    </div>
-  )
-}
+    <>
+      <HomeMenu />
 
-export default Home
+      <div className='home-container'>
+        <div className='home-content'>
+          <Container maxWidth="md" sx={{ textAlign: 'center', mt: 20, mb: 10 }}>
+            <Typography variant="h3" component="h1" gutterBottom fontWeight="bold" color='primary'>
+              Welcome to Dhananjaya's App
+            </Typography>
+            <Typography variant="h6" color="text.secondary" paragraph>
+              This is your homepage. Use the navigation bar to sign in or create a new account.
+            </Typography>
+            <Button
+              variant="contained"
+              size="large"
+              component={RouterLink}
+              to="/signup"
+              sx={{ mt: 4 }}
+            >
+              Get Started
+            </Button>
+            <Box sx={{ textAlign: 'center', mt: 4 }}>
+              <Typography variant="body2" color="text.secondary">
+                © {new Date().getFullYear()} Dhananjaya. All rights reserved.
+              </Typography>
+            </Box>
+          </Container>
+        </div>
+      </div>
+
+      {/* <Container  className='home-container'>
+        <Box sx={{ my: 8, textAlign: 'center' }} className='home-content'> 
+          <Typography variant="h3" component="h1" gutterBottom fontWeight="bold" color='primary'>
+            Welcome to Dhananjaya's App
+          </Typography>
+          <Typography variant="h6" color="text.secondary" paragraph>
+            This is your homepage. Use the navigation bar to sign in or create a new account.
+          </Typography>
+          <Button
+            variant="contained"
+            size="large"
+            component={RouterLink}
+            to="/signup"
+            sx={{ mt: 4 }}
+          >
+            Get Started
+          </Button>
+        </Box>
+        <Box sx={{ textAlign: 'center', mt: 4 }}>
+          <Typography variant="body2" color="text.secondary">
+            © {new Date().getFullYear()} Dhananjaya. All rights reserved.
+          </Typography>
+        </Box>
+      </Container> */}
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/HomeMenu.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+const HomeMenu = () => {
+  return (
+    <AppBar color="primary" position='fixed'>
+      <Toolbar>
+        <Typography variant="h6" component={RouterLink} to="/" sx={{ flexGrow: 1, color: 'inherit', textDecoration: 'none' }}>
+          Dhananjaya
+        </Typography>
+        <Button color="inherit" component={RouterLink} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={RouterLink} to="/signin">
+          Sign In
+        </Button>
+        <Button color="inherit" component={RouterLink} to="/signup">
+          Sign Up
+        </Button>
+        <Button color="inherit" component={RouterLink} to="/dashboard">
+          Dashboard
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default HomeMenu;
